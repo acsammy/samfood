2590cd18537d7617540b15b254f4ddb3e94c874b
Samuel Carvalho <carvalho.asamuel@gmail.com>
2019-05-02 22:28:26 -0300

    Adding relationships between models

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,4 @@
 class Order < ApplicationRecord
+  has_many :order_products
   belongs_to :restaurant
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,6 @@
 class Restaurant < ApplicationRecord
+  has_many :product_categories
+  has_many :orders
+  has_many :reviews
   belongs_to :category
 end
