e762ad2c7915d64dd31d835ba20f61d83be2a785
Samuel Carvalho <carvalho.asamuel@gmail.com>
2019-05-02 22:49:44 -0300

    Added model's validation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   has_many :order_products
   belongs_to :restaurant
+
+  validates :name, presence: true
+  validates :phone_number, presence: true
+  validates :total_value, presence: true
+
+  enum :status {waiting: 0, delivered: 1}
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   has_many :order_products
   belongs_to :product_category
+
+  validates_associated :product_category
+  validates :name, presence: true
+  validates :price, presence: true
 end
