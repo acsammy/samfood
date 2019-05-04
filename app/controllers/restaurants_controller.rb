7d9f8fa08052fce93cff201f944a865e4b011ee9
Samuel Carvalho <carvalho.asamuel@gmail.com>
2019-05-04 12:40:16 -0300

    Adde controllers data

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,26 @@
 class RestaurantsController < ApplicationController
+  before_action :set_restaurant, only :show
   def index
+    @restaurants = Restaurant.near(params[:city] || 'São Pualo')
+    filter_by_category if params[:category]
+    render json: @restaurants
   end
 
   def show
+    render json: @restaurants, product_categories: true
   end
 
   def search
   end
+
+  private
+    def set_restaurant
+      @restaurants = Restaurant.find_by(:id, params[:id])
+    end
+
+    def filter_by_category
+      @restaurants = @restaurants.select do |r|
+        r.category.title == params[:category]
+      end
+    end
 end
